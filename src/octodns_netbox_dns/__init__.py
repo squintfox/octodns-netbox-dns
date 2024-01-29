3a89cf56dc174f98757be12727703b59c672cbd5
Robert Spadaro <55590532+squintfox@users.noreply.github.com>
2024-01-29 16:03:00 -0500

    Escape after

--- src/octodns_netbox_dns/__init__.py
+++ src/octodns_netbox_dns/__init__.py
@@ -209,7 +209,7 @@ class NetBoxDNSSource(octodns.provider.base.BaseProvider):
                 }
 
             case "SPF" | "TXT":
-                value = raw_value.replace("\\\\", "\\")
+                value = raw_value.replace("\\\\", "\\").replace(";", r"\;")
 
             case "SRV":
                 value = {
